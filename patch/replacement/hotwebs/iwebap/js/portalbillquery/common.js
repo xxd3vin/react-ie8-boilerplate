b0f4d5e777dce958c3fffac14be852ab7a0ea666
Devin Chen <d3vin.chen@gmail.com>
2016-12-20 17:00:40 +0800

    test

---
+++ patch/replacement/hotwebs/iwebap/js/portalbillquery/common.js
@@ -0,0 +1,10 @@
+/*!
+ * 
+ *    react-ie8-boilerplate - 
+ *    Author: tommyshao
+ *    Version: v1.0.0
+ *    URL: http://github.com/tomieric/react-ie8-boilerplate
+ *    License(s): MIT
+ * 
+ */
+!function(e){function t(r){if(n[r])return n[r].exports;var l=n[r]={exports:{},id:r,loaded:!1};return e[r].call(l.exports,l,l.exports,t),l.loaded=!0,l.exports}var r=window.webpackJsonp;window.webpackJsonp=function(a,o){for(var p,i,s=0,c=[];s<a.length;s++)i=a[s],l[i]&&c.push.apply(c,l[i]),l[i]=0;for(p in o)e[p]=o[p];for(r&&r(a,o);c.length;)c.shift().call(null,t);if(o[0])return n[0]=0,t(0)};var n={},l={1:0};t.e=function(e,r){if(0===l[e])return r.call(null,t);if(void 0!==l[e])l[e].push(r);else{l[e]=[r];var n=document.getElementsByTagName("head")[0],a=document.createElement("script");a.type="text/javascript",a.charset="utf-8",a.async=!0,a.src=t.p+""+e+"."+({2:"js/portalbillquery/MyExpBillPortlet"}[e]||e)+".js",n.appendChild(a)}},t.m=e,t.c=n,t.p=""}([]);
